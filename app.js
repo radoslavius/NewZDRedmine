b984b97ed42a49622df23082d595a878e1c26e72
Radoslav Stofko <rstofko@pointcz.com>
2016-01-15 13:02:04 +0100

    new features - change assigned to - set custom fields to redmine (working on)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 (function(){
   var PROJECT_STATUS_ACTIVE = 1;
   return {
+    defaultState: 'loading',
     PROJECT_TO_USE: '1',
     TRACKERS: [],
     PROJECTS: [],
@@ -60,6 +61,15 @@
           dataType: 'json',
           secure: true
         };
+      },
+      getMembers: function(redmine_url)
+      {
+        return {
+          url: redmine_url+'/projects/'+this.PROJECT_TO_USE+'/memberships.json?key={{setting.apiKey}}',
+          type: 'GET',
+          dataType: 'json',
+          secure: true
+        };
       }
    },
     events: {
@@ -68,11 +78,13 @@
       'click #submitToRedmine'  : 'fn_prep_to_post',
       'getProjects.done'        : 'fn_listProjects',
       'getTrackers.done'        : 'fn_saveTrackers',
+      'getMembers.done'         : 'fn_saveMembers',
       'getAudit.done'           : 'fn_listMeta',
       'click .project'          : 'fn_projectSelect',
       'updateTicket.done'       : 'fn_reset',
       'click .issue'            : 'fn_get_issue',
       'getIssue.done'           : 'fn_show_issue',
+      'getIssue.fail'           : 'fn_show_issue_error',
       'click .back_button'      : 'onActivated'
     },
     fn_renderError: function(error_text)
@@ -81,7 +93,6 @@
       this.switchTo('error', {error: error_text});
     },
     onActivated: function(){
-      console.log('app: 1.0');
       this.doneLoading = false;
       this.loadIfDataReady();
     },
@@ -127,24 +138,76 @@
       var subject = this.$('#rm_subject').val();
       var tracker = this.$('#rm_tracker').val();
       var priority = this.$('#rm_priority').val();
+      var assigned = this.$('#rm_assigned').val();
       if(subject.length < 1){
         services.notify('You must include a subject.', 'error');
       }else{
+        this.switchTo('loading');
         var ticket_desc = this.ticket().description();
         ticket_desc = ticket_desc.replace( /&/gim, '' ).replace( /</gim, '').replace( />/gim, '').replace(/:/gim, '');
-        var data = {"issue": {"subject": subject, "project_id": this.PROJECT_TO_USE, "tracker_id": tracker, "description": "This issue was pushed from Zendesk to Redmine.\n---\n\nDescription:\n"+ticket_desc+"\n---\n\nAdditional Message from Zendesk\n---\n"+this.$('#rm_note').val()+"\n\nTicket URL: https://"+this.currentAccount().subdomain()+".zendesk.com/tickets/"+this.ticket().id()+"\n\n"}};
-        this.ajax('postRedmine', this.settings.project, this.settings.redmine_url, data);
+        var data = {
+          "issue": {
+            "subject": subject,
+            "project_id": this.PROJECT_TO_USE,
+            "tracker_id": tracker,
+            "assigned_to_id": assigned,
+            "description": "This issue was pushed from Zendesk to Redmine.\n---\n\nDescription:\n"+ticket_desc+"\n---\n\nAdditional Message from Zendesk\n---\n"+this.$('#rm_note').val()+"\n\nTicket URL: https://"+this.currentAccount().subdomain()+".zendesk.com/tickets/"+this.ticket().id()+"\n\n"
+          }
+        };
+        if(this.settings.customFields.length > 0 && this.isJsonString(this.settings.customFields)) {
+          data.issue.custom_fields = JSON.parse(this.settings.customFields);
+        }
+        else {
+          data.issue.custom_fields = [];
+        }
+        if(this.settings.customFieldsIssueId > 0) {
+          data.issue.custom_fields.push(
+            {"id":this.settings.customFieldsIssueId,"value":this.ticket().id()}
+          );
+        }
+        //data.issue.custom_fields = JSON.parse('[{"value":"Zendesk","id":7},{"value":194,"id":13}]');
+        this.ajax('postRedmine', this.settings.project, this.settings.redmine_url, JSON.parse(JSON.stringify(data)));
       }
     },
     fn_projectSelect: function(e)
     {
+      this.switchTo('loading');
       this.PROJECT_TO_USE = e.target.id;
+      this.MEMBERS = [];
       this.ajax('getTrackers', this.settings.redmine_url);
     },
     fn_saveTrackers: function(data)
     {
       this.TRACKERS = data.project;
-      this.switchTo('index', {track: this.TRACKERS});
+      this.ajax('getMembers', this.settings.redmine_url);
+      //this.switchTo('index', {track: this.TRACKERS, assign: this.MEMBERS});
+    },
+    fn_saveMembers: function(data)
+    {
+      var members = data.memberships;
+      var solvers = [];
+      var self = this;
+      members.forEach(function(member) {
+        var can_solve = false;
+        member.roles.forEach(function(role) {
+          if(role.id == self.settings.roleId) {
+            can_solve = true;
+          }
+        });
+        if(can_solve) {
+          solvers.push(member);
+        }
+      });
+      this.MEMBERS = solvers;
+
+      if(this.MEMBERS.length > 0) {
+        this.switchTo('index', {track: this.TRACKERS, assign: this.MEMBERS});
+      }
+      else {
+        this.switchTo('error', {error: "No user found for assignment"});
+      }
+
+      //this.switchTo('index', {track: this.TRACKERS, assign: this.MEMBERS});
     },
     fn_listMeta: function(data)
     {
@@ -170,14 +233,27 @@
       }
     },
     fn_reset: function(){
+      this.switchTo('loading');
       this.ajax('getProjects', this.settings.redmine_url);
     },
     fn_get_issue: function(e){
+      this.switchTo('loading');
       var issue_id = e.target.classList[1].replace("id_", "");
       this.ajax('getIssue', this.settings.redmine_url, issue_id);
     },
     fn_show_issue: function(data){
       this.switchTo('show_issue', {issue: data.issue, url: this.settings.redmine_url+"/issues/"+data.issue.id });
+    },
+    fn_show_issue_error: function(data){
+      this.fn_renderError('Issue was not found!');
+    },
+    isJsonString: function(str) {
+      try {
+        JSON.parse(str);
+      } catch (e) {
+        return false;
+      }
+      return true;
     }
   };
 }());    
